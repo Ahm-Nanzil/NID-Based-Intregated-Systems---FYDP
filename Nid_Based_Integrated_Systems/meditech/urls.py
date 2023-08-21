849e6671baf72a0980c52b1140272aa7596d157e
Farhan Shahriar Alvi <52394409+FarhanShahriaralvi@users.noreply.github.com>
2023-08-22 01:57:43 +0600

    link all file to other file

--- Nid_Based_Integrated_Systems/meditech/urls.py
+++ Nid_Based_Integrated_Systems/meditech/urls.py
@@ -4,6 +4,14 @@ from meditech import views
 
 urlpatterns = [
     path("", views.index, name='home') ,
-    path("doctor", views.doctor, name='doctor')
+    path("doctorviewpatientreport", views.doctorvieewpatientreport, name='doctorvieewpatientreport'),
+    path("doctor", views.doctor, name='doctor'),
+    path("medication", views.medication, name='medication'),
+    path("report", views.report, name='report'),
+    path("newmedication", views.newmedication, name='newmedication'),
+    path("patient", views.patient, name='patient'),
+    path("phermacy", views.phermacy, name='phermacy'),
+    path("diagonesis", views.diagonesis, name='diagonesis'),
+
       
 ]
--- Nid_Based_Integrated_Systems/meditech/views.py
+++ Nid_Based_Integrated_Systems/meditech/views.py
@@ -5,5 +5,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+def doctorviewpatientreport(request):
+    return render(request, 'doctor-view-patientreport.html')
+
 def doctor(request):
     return render(request, 'doctor-view.html')
+def medication(request):
+    return render(request, 'view-medication.html')
+def report(request):
+    return render(request, 'view-report.html')
+def newmedication(request):
+    return render(request, 'new-medication.html')
+
+def patient(request):
+    return render(request, 'patient-view.html')
+def phermacy(request):
+    return render(request, 'phermacy-view.html')
+def diagonesis(request):
+    return render(request, 'diagonesis-view.html')
